ee640fc211b5831e580597e27d3186d947646587
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-01-26 20:48:19 -0600

    Example of numericalunits calculation (finally).

---
+++ deflection_numericalunits.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+import math
+import numericalunits as nu
+nu.reset_units()
+
+def deflection_1(tangent_length, radius):
+    return math.hypot(tangent_length, radius) - radius
+
+def deflection_2(arc_length, radius):
+    return radius * (math.cos(arc_length/radius)**-1 - 1)
+
+radius_earth = 6371.01 * nu.km
+length = 1.0 * nu.km
+
+deflection_1 = deflection_1(length, radius_earth) / nu.cm
+
+deflection_2 = deflection_2(length, radius_earth) / nu.cm
+
+with open("deflection_numericalunits.out", 'w') as f:
+    f.write(str(deflection_1) + ' cm\n')
+    f.write(str(deflection_2) + ' cm\n')
