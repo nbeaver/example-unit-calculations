64f86dddc4ae03a6925f5b3c0e5528f2e0464de2
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-01-26 21:35:47 -0600

    Alter function to make Pint work.

--- deflection_Pint.py
+++ deflection_Pint.py
@@ -4,10 +4,14 @@ import os
 import math
 from pint import UnitRegistry
 
-def deflection_1(tangent_length, radius):
-    return math.hypot(tangent_length, radius) - radius
+def deflection_tangent(tangent_length, radius):
+    return (tangent_length**2 + radius**2)**(1.0/2.0) - radius
+    # Cannot use math.hypot:
+    # pint.unit.DimensionalityError: Cannot convert from 'kilometer' to 'dimensionless'
+    # Cannot use math.sqrt:
+    # pint.unit.DimensionalityError: Cannot convert from 'kilometer ** 2' to 'dimensionless'
 
-def deflection_2(arc_length, radius):
+def deflection_arc(arc_length, radius):
     return radius * (math.cos(arc_length/radius)**-1 - 1)
 
 ureg = UnitRegistry()
@@ -15,10 +19,10 @@ ureg = UnitRegistry()
 radius_earth = 6371.01 * ureg.kilometers
 length = 1.0 * ureg.kilometers
 
-#deflection_1 = deflection_1(length, radius_earth) # fails
+deflection_1 = deflection_tangent(length, radius_earth).to(ureg.centimeters)
 
-deflection_2 = deflection_2(length, radius_earth)
+deflection_2 = deflection_arc(length, radius_earth).to(ureg.centimeters)
 
 with open(os.path.splitext(__file__)[0]+'.out', 'w') as f:
-    #f.write(str(deflection_1) + '\n')
+    f.write(str(deflection_1) + '\n')
     f.write(str(deflection_2) + '\n')
