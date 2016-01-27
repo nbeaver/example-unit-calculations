c9027a53c6c952ac425f4eee428c0aafb0d8a61a
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-01-27 01:15:01 -0600

    Trying one more way to get this to work and failing.

--- deflection_units.py
+++ deflection_units.py
@@ -2,6 +2,7 @@
 
 import os
 import math
+import fractions
 from units import unit
 from units.predefined import define_units
 define_units()
@@ -22,6 +23,11 @@ def deflection_tangent4(tangent_length, radius):
     return (tangent_length**2 + radius**2)**(1.0/2.0) - radius
     # TypeError: can't multiply sequence by non-int of type 'float'
 
+def deflection_tangent5(tangent_length, radius):
+    half = fractions.Fraction(1, 2)
+    return (tangent_length**2 + radius**2)**half - radius
+    # TypeError: can't multiply sequence by non-int of type 'Fraction'
+
 def deflection_arc(arc_length, radius):
     return radius * (math.cos(arc_length/radius)**-1 - 1)
 
@@ -32,6 +38,7 @@ length = unit('km')(1.0)
 #deflection_1 = unit('cm')(deflection_tangent2(length, radius_earth))
 #deflection_1 = unit('cm')(deflection_tangent3(length, radius_earth))
 #deflection_1 = unit('cm')(deflection_tangent4(length, radius_earth))
+#deflection_1 = unit('cm')(deflection_tangent5(length, radius_earth))
 
 deflection_2 = unit('cm')(deflection_arc(length, radius_earth))
 
