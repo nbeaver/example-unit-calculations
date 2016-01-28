17967f542cb2b617a0b2588471f8da2b315b8544
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-01-28 10:38:18 -0600

    Show what the GNU units calculations are like.

---
+++ planck-units/gnu-units.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+
+units --terse 'sqrt(hbar G / c^3)'
+units --terse 'sqrt(hbar c / G)'
+units --terse 'sqrt(hbar G / c^5)'
+units --terse 'sqrt(4 pi epsilon0 hbar c)'
+units --terse 'sqrt(hbar c^5 / G kboltzmann^2)'
