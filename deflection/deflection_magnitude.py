8ed1e62ba6b7ec576e35ac574d79dad114f13a72
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-02-02 00:08:36 -0600

    Forget to actually do this one last time.

--- deflection/deflection_magnitude.py
+++ deflection/deflection_magnitude.py
@@ -3,21 +3,22 @@ from __future__ import print_function
 
 import os
 import math
+from magnitude import mg
 
 def deflection_tangent(tangent_length, radius):
-    return math.hypot(tangent_length, radius) - radius
+    return (tangent_length**2 + radius**2)**(1.0/2.0) - radius
 
 def deflection_arc(arc_length, radius):
     return radius * (math.cos(arc_length/radius)**-1 - 1)
 
-radius_earth = 6371.01 # km
-length = 1.0 # km
+radius_earth = mg(6371.01, 'km')
+length = mg(1, 'km')
 
-km_to_cm = 1e3/1e-2
+deflection_1 = deflection_tangent(length, radius_earth).ounit('cm')
+deflection_1.output_prec(10)
 
-deflection_1 = km_to_cm * deflection_tangent(length, radius_earth)
-
-deflection_2 = km_to_cm * deflection_arc(length, radius_earth)
+deflection_2 = deflection_arc(length, radius_earth).ounit('cm')
+deflection_2.output_prec(10)
 
 print(deflection_1)
 print(deflection_2)
