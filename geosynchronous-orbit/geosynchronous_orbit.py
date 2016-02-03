1286ed49ab3e994d29ec61d678feceda5c7ec252
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-02-03 15:56:56 -0600

    Put orbit calculation into separate function and convert to km.

--- geosynchronous-orbit/geosynchronous_orbit.py
+++ geosynchronous-orbit/geosynchronous_orbit.py
@@ -4,12 +4,17 @@ from __future__ import print_function
 import os
 import math
 
-G = 6.67384e-11 # m^3 / kg s^2
+def orbital_radius(M, omega):
+    G = 6.67384e-11 # m^3 / kg s^2
+    return (G * M / omega**2)**(1.0/3.0)
+
 mass_earth = 5.9742412e+24 # kg
 revolution = 2 * math.pi # radian
 siderealday = 86164.09054 # s
 radius_earth = 6378136.49 # m
 
-orbital_altitude = (G * mass_earth / (revolution/siderealday)**2)**(1.0/3.0) - radius_earth
+orbital_altitude = orbital_radius(mass_earth, revolution/siderealday) - radius_earth
 
-print(orbital_altitude, 'm')
+m_to_km = 1.0/1000.0
+
+print(orbital_altitude * m_to_km, 'km')
