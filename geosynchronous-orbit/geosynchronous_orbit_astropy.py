820791dbe288d8ea2e233af1d0defc0abe22cf03
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-02-03 16:25:18 -0600

    Make it less compact, but easier to compare with original.

--- geosynchronous-orbit/geosynchronous_orbit_astropy.py
+++ geosynchronous-orbit/geosynchronous_orbit_astropy.py
@@ -6,7 +6,8 @@ from astropy.units import cds
 cds.enable()
 
 def orbital_radius(M, omega):
-    return (cds.G * M / omega**2)**(1.0/3.0)
+    G = cds.G
+    return (G * M / omega**2)**(1.0/3.0)
 
 revolution = u.cycle.in_units(u.radian)
 
